5cbd2ba1bc54798102c90962ac95fb8d4a0c956d
karantambat11 <karantambat11@gmail.com>
2025-05-07 20:04:24 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,12 +41,12 @@ if uploaded_file:
         chart = alt.Chart(filtered_data).mark_line(point=True).encode(x='Date', y=alt.Y('Average Price', title='Average Price'), color=alt.value('blue')).properties(title=f'Average Price Trend for {subcategory}')
         st.altair_chart(chart, use_container_width=True)
 
-            chart_gm = alt.Chart(filtered_data).mark_line(point=True).encode(
-                x='Date', 
-                y=alt.Y('Gross Margin (%)', title='Gross Margin (%)'), 
-                color=alt.value('red')
-            ).properties(title=f'Gross Margin Trend for {subcategory}')
-            st.altair_chart(chart_gm, use_container_width=True)
+        chart_gm = alt.Chart(filtered_data).mark_line(point=True).encode(
+            x='Date', 
+            y=alt.Y('Gross Margin (%)', title='Gross Margin (%)'), 
+            color=alt.value('red')
+        ).properties(title=f'Gross Margin Trend for {subcategory}')
+        st.altair_chart(chart_gm, use_container_width=True)
 
         # Top and Bottom SKUs
         st.subheader("Top and Bottom SKUs by Gross Margin")
